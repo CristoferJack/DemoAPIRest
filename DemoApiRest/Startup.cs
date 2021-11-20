b16b0d840afc60514480cb46a240c6672f3ea5d6
Cristofer Bardales V <cristofer.bv@outlook.com>
2021-11-20 15:22:36 -0500

    Creacion servicios Docker

--- DemoApiRest/Startup.cs
+++ DemoApiRest/Startup.cs
@@ -32,6 +32,8 @@ namespace DemoApiRest
             Configuration = builder.Build();
         }
 
+        string corsPolicy = "AllowAllOrigins";
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -41,11 +43,29 @@ namespace DemoApiRest
 
             services.AddTransient<IPersonaRepository, PersonaRepository>().AddTransient<IPersonaService, PersonaService>();
 
-            services.AddDbContext<DemoDbContext>(options =>
-            {
-                options.UseSqlServer(Configuration.GetConnectionString
-                    ("Conexion"));
+            services.AddCors(options => {
+                options.AddPolicy(corsPolicy, builder =>
+                {
+                    builder.AllowAnyOrigin();
+                });
             });
+
+            /* Conexion con Json file */
+            //services.AddDbContext<DemoDbContext>(options =>
+            //{
+            //    options.UseSqlServer(Configuration.GetConnectionString
+            //        ("Conexion"));
+            //});
+
+            /* Variables y Conexion por defecto */
+            var server = Environment.GetEnvironmentVariable("SERVER") ?? "192.168.0.13\\SQLEXPRESS";
+            var database = Environment.GetEnvironmentVariable("DATABASE") ?? "DemoApiRestDB";
+            var user = Environment.GetEnvironmentVariable("USER") ?? "sa";
+            var password = Environment.GetEnvironmentVariable("PASSWORD") ?? "123456";
+
+            services.AddDbContext<DemoDbContext>(options =>
+                options.UseSqlServer($"Server={server};Initial Catalog={database};User ID={user};Password={password};Integrated Security=True;Trusted_Connection=False"));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,6 +86,8 @@ namespace DemoApiRest
             {
                 endpoints.MapControllers();
             });
+
+            app.UseCors(corsPolicy);
         }
     }
 }
